9f0334ca5d7651c16426d316ad15ec74ec755173
zaidur <rahmanzaidur@gmail.com>
2022-04-20 17:54:55 -0400

    My First Selenium Project

---
+++ src/test/java/Class1/UseSelenium.java
@@ -0,0 +1,27 @@
+package Class1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class UseSelenium {
+
+
+    @Test
+    public void UseSeleniumLib() {
+
+        System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com/");
+
+    }
+
+}
+
+
+
+
+
+
+
